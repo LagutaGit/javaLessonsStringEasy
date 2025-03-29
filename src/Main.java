55f9f4ee7ebdcd7e8ef091d676ec08255b0d6a0c
LagutaGit <roma_laguta@mail.ru>
2025-03-30 00:27:51 +0900

    Поправил лекцию, добавил проверку -, а также чтобы номер начинался с 7

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public void lection(){
-        String phone = "800553535";
+        String phone = "800-555-35-35";
+//        Добавляю проверку если ввели -
+        phone = phone.replace("-", "");
         if (phone.length() == 10) {
             phone = "7" + phone;
         } else if (phone.length() > 11){
@@ -9,8 +11,10 @@ public class Main {
         }
         else if (phone.length() <11) {
             throw new RuntimeException("Короткий");
-        }
-        else {
+//            Добавляю проверку если не с 7 начинается
+        } else if (phone.length() == 11 && phone.charAt(0) != '7') {
+            throw new RuntimeException("Среди нас посторонний");
+        } else {
             throw new RuntimeException("Ошибка ввода");
         }
         System.out.println("phone = " + phone);
