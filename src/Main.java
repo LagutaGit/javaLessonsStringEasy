a202c7dc0556a76666ca46ad8867b5bb43ab9664
LagutaGit <roma_laguta@mail.ru>
2025-03-30 00:15:55 +0900

    Прописал лекцию

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public void lection(){
+        String phone = "800553535";
+        if (phone.length() == 10) {
+            phone = "7" + phone;
+        } else if (phone.length() > 11){
+            throw new RuntimeException("Телефон слишком длинный");
+        }
+        else if (phone.length() <11) {
+            throw new RuntimeException("Короткий");
+        }
+        else {
+            throw new RuntimeException("Ошибка ввода");
+        }
+        System.out.println("phone = " + phone);
+    }
+
+
+    public static void main(String[] args) {
+//        lection();
     }
 }
