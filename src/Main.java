ba7aafe8cc3f554f474cd50a5edacb9b96bd0ae0
LagutaGit <roma_laguta@mail.ru>
2025-03-31 21:51:47 +0900

    Сделал Домашку по строкам

--- src/Main.java
+++ src/Main.java
@@ -22,8 +22,38 @@ public class Main {
         System.out.println("phone = " + phone);
     }
 
+    public static void homeWork1(){
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+        char first = firstName.charAt(0);
+        char second = lastName.charAt(0);
+        char thrid = middleName.charAt(0);
+        System.out.println("Ф. И. О. сотрудника —" + first + second + thrid);
+    }
+
+    public static void homeWork2(){
+        String fullName = "ivanov ivan ivanovich";
+        String fullNameFIO = fullName.toUpperCase();
+        System.out.println("fullNameFIO = " + fullNameFIO);
+    }
+
+    public static void homeWork3(){
+        String fio = "Иванов Семён Семёнович";
+        String withoutYo = fio.replace("ё", "е");
+        System.out.println("withoutYo = " + withoutYo);
+    }
+
+    public static void homeWork4(){
+
+    }
 
     public static void main(String[] args) {
-        lection();
+//        lection();
+        homeWork1();
+        homeWork2();
+        homeWork3();
+        homeWork4();
     }
 }
